01795a164d48612d203cdb946ecfc04e128b3b1a
maxxyouu <max.you@outlook.com>
2024-12-06 10:19:57 -0500

    progress

--- scripts/nii_to_embeddings.py
+++ scripts/nii_to_embeddings.py
@@ -16,6 +16,44 @@ import accelerate
 import preprocess_utils
 from zero_shot import CTClipInference
 from pathlib import Path
+from functools import partial
+from multiprocessing import Pool
+
+def download_and_move(file, destination_folder, repo_id):
+    # Extract the filename from the path
+    filename = os.path.basename(file)
+    dir_path = os.path.dirname(file)
+    
+    # Normalize each component to use consistent separators
+    dir_path = os.path.normpath(dir_path)
+    
+    # Ensure the destination directory exists
+    destination_dir = os.path.join(destination_folder, dir_path)
+    os.makedirs(destination_dir, exist_ok=True)
+
+    # Move the downloaded file to the destination folder
+    file_dir = os.path.join(destination_dir, filename)
+
+
+    # Download the file
+    file_path = hf_hub_download(
+        repo_id=repo_id,
+        filename=filename,
+        subfolder=dir_path,
+        repo_type="dataset",
+        cache_dir='/mnt/f/cache', # must be placed in the same external hard drive for wsl to work for shutil.move operations
+        token='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' # Ensure your token is set in the huggingface
+    )
+
+    shutil.move(file_path, file_dir)
+
+def parallel_download(batch, destination_folder, repo_id, num_workers=8):
+    with Pool(num_workers) as pool:
+        # Prepare the partial function with fixed arguments
+        func_with_args = partial(download_and_move, destination_folder=destination_folder, repo_id=repo_id)
+
+        # Process the files in parallel with a progress bar
+        list(tqdm(pool.imap_unordered(func_with_args, batch), total=len(batch)))
 
 if __name__ == '__main__':
 
@@ -53,7 +91,7 @@ if __name__ == '__main__':
     split = 'train'
 
     # perform feature extraction after download 100 of them
-    feature_extraction_frequency = 2000
+    feature_extraction_frequency = 10
 
     repo_id = "ibrahimhamamci/CT-RATE"
     folder_path = "dataset/{}".format(split)
@@ -77,50 +115,24 @@ if __name__ == '__main__':
     files = [f for f in files if Path(Path(f).stem).stem not in keys] # nested path.stem due to .nii.gz, each remove one extension
 
     print(f"Files in the '{split}' folder: {len(files)}")
-    total_files = len(files)
+    total_files = 20 #len(files)
 
     destination_folder = os.path.normpath('/mnt/f/Chris/CT-RATE-temp')
     os.makedirs(destination_folder, exist_ok=True)
 
     for i in tqdm(range(0, total_files, feature_extraction_frequency)):
         batch = files[i:i + feature_extraction_frequency]
+        print('    downloading files\n')
+        parallel_download(batch, destination_folder, repo_id, num_workers=8)
 
-        for file in batch:
-            # Extract the filename from the path
-            filename = os.path.basename(file)
-            dir_path = os.path.dirname(file)
-            
-            # Normalize each component to use consistent separators
-            dir_path = os.path.normpath(dir_path)
-            
-            # Ensure the destination directory exists
-            destination_dir = os.path.join(destination_folder, dir_path)
-            os.makedirs(destination_dir, exist_ok=True)
-
-            # Move the downloaded file to the destination folder
-            file_dir = os.path.join(destination_dir, filename)
-
-
-            # Download the file
-            file_path = hf_hub_download(
-                repo_id=repo_id,
-                filename=filename,
-                subfolder=dir_path,
-                repo_type="dataset",
-                cache_dir='/mnt/f/cache', # must be placed in the same external hard drive for wsl to work for shutil.move operations
-                token='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' # Ensure your token is set in the huggingface
-            )
-
-            shutil.move(file_path, file_dir)
-
-        print('    processing raw ct files')
+        print('    processing raw ct files\n')
         # NOTE: preprocess the downloaded files and save the corresponding xray
         raw_ct_path = os.path.join(destination_folder, 'dataset', f'{split}') # load the data for processing from here
         processed_ct_dest = os.path.join(destination_folder, 'processed_dataset') # destination folder to hold the processed ct and xray files
-        preprocess_utils.process(nii_path=raw_ct_path, shared_dest=processed_ct_dest, split=split)
+        preprocess_utils.process(nii_path=raw_ct_path, shared_dest=processed_ct_dest, split=split, num_workers=8)
 
         # NOTE: remove the raw CT files and keep the xray files
-        print('    removing raw ct files')
+        print('    removing raw ct files\n')
         shutil.rmtree(raw_ct_path)
 
         # start to do feature extraction
@@ -137,11 +149,11 @@ if __name__ == '__main__':
         )
 
         # feature extraction save the features to the phe object
-        print('    performing feature extraction')
+        print('    performing feature extraction\n')
         inference.feature_extraction('/mnt/f/Chris/dataset/features_embeddings', f'{split}')
 
         #NOTE: remove the preprocessed ct files ONLY
-        print('    removing processed ct files')
+        print('    removing processed ct files\n')
         shutil.rmtree(processed_ct_directory)
         
     print("Finished")
--- scripts/preprocess_utils.py
+++ scripts/preprocess_utils.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 import SimpleITK as sitk
 from PIL import Image
 from functools import partial
+import shutil
+
 
 def read_nii_files(directory):
     """
@@ -102,119 +104,124 @@ def convert_ct_to_xray(file_path, df, split='train', shared_dst_dir='F:\\Chris\\
     file_name = file_name.split(".")[0]+".png"
     xray_rgb_save_path = os.path.join(xray_folder_path_new, file_name)
 
+    try:
 
-    img_data = read_nii_data(file_path)
-    if img_data is None:
-        print(f"Read {file_path} unsuccessful. Passing")
-        return
+        img_data = read_nii_data(file_path)
+        if img_data is None:
+            print(f"Read {file_path} unsuccessful. Passing")
+            return
 
-    row = df[df['VolumeName'] == original_file_name]
-    slope = float(row["RescaleSlope"].iloc[0])
-    intercept = float(row["RescaleIntercept"].iloc[0])
-    xy_spacing = float(row["XYSpacing"].iloc[0][1:][:-2].split(",")[0])
-    z_spacing = float(row["ZSpacing"].iloc[0])
+        row = df[df['VolumeName'] == original_file_name]
+        slope = float(row["RescaleSlope"].iloc[0])
+        intercept = float(row["RescaleIntercept"].iloc[0])
+        xy_spacing = float(row["XYSpacing"].iloc[0][1:][:-2].split(",")[0])
+        z_spacing = float(row["ZSpacing"].iloc[0])
 
-    # Define the target spacing values
-    target_x_spacing = 0.75
-    target_y_spacing = 0.75
-    target_z_spacing = 1.5
+        # Define the target spacing values
+        target_x_spacing = 0.75
+        target_y_spacing = 0.75
+        target_z_spacing = 1.5
 
-    def _scale_clip_resize(nii_data, current, target):
+        def _scale_clip_resize(nii_data, current, target):
 
-        # scale
-        _img_data = slope * nii_data + intercept
+            # scale
+            _img_data = slope * nii_data + intercept
 
-        # clip
-        hu_min, hu_max = -1000, 1000
-        _img_data = np.clip(_img_data, hu_min, hu_max)
-        _img_data = (((_img_data ) / 1000)).astype(np.float32) # as float is important
+            # clip
+            hu_min, hu_max = -1000, 1000
+            _img_data = np.clip(_img_data, hu_min, hu_max)
+            _img_data = (((_img_data ) / 1000)).astype(np.float32) # as float is important
 
-        _img_data = _img_data.transpose(2, 0, 1) # z, x, y
-        ct_tensor = torch.tensor(_img_data)
-        ct_tensor = ct_tensor.unsqueeze(0).unsqueeze(0)
+            _img_data = _img_data.transpose(2, 0, 1) # z, x, y
+            ct_tensor = torch.tensor(_img_data)
+            ct_tensor = ct_tensor.unsqueeze(0).unsqueeze(0)
 
-        # resize
-        _img_data = resize_array(ct_tensor, current, target)
-        _img_data = _img_data[0][0]
-        _img_data= np.transpose(_img_data, (1, 2, 0)) # xyz
-        _img_data = _img_data*1000
+            # resize
+            _img_data = resize_array(ct_tensor, current, target)
+            _img_data = _img_data[0][0]
+            _img_data= np.transpose(_img_data, (1, 2, 0)) # xyz
+            _img_data = _img_data*1000
 
-        return _img_data
+            return _img_data
 
-    current = (z_spacing, xy_spacing, xy_spacing)
+        current = (z_spacing, xy_spacing, xy_spacing)
 
-    ct_image = _scale_clip_resize(img_data, current, (target_z_spacing, target_x_spacing, target_y_spacing))
-    xray_image = _scale_clip_resize(img_data, current, (1,1,1))
+        ct_image = _scale_clip_resize(img_data, current, (target_z_spacing, target_x_spacing, target_y_spacing))
+        xray_image = _scale_clip_resize(img_data, current, (1,1,1))
 
-    #TEST
-    # sitk.WriteImage(sitk.GetImageFromArray(ct_image), './test_{}'.format(original_file_name))
-    
-    # for xray
-    xray_image = sitk.GetImageFromArray(xray_image)
-    mean_projection_filter = sitk.MeanProjectionImageFilter()
-    mean_projection_filter.SetProjectionDimension(1)
-    xray_image = mean_projection_filter.Execute(xray_image) # execute projection
+        #TEST
+        # sitk.WriteImage(sitk.GetImageFromArray(ct_image), './test_{}'.format(original_file_name))
+        
+        # for xray
+        xray_image = sitk.GetImageFromArray(xray_image)
+        mean_projection_filter = sitk.MeanProjectionImageFilter()
+        mean_projection_filter.SetProjectionDimension(1)
+        xray_image = mean_projection_filter.Execute(xray_image) # execute projection
 
-    #TODO: not sure why we need manual flipping here to match nii image for the frontal view
-    xray_array = sitk.GetArrayFromImage(xray_image)
-    # xray_array = np.flip(np.squeeze(xray_array), axis=0)
-    xray_array = np.rot90(np.squeeze(xray_array)) # make the image upright but NOTE that it is flipped with respect to the y-axis
-    # xray_array = np.squeeze(xray_array) # make the image upright but NOTE that it is flipped with respect to the y-axis
+        #TODO: not sure why we need manual flipping here to match nii image for the frontal view
+        xray_array = sitk.GetArrayFromImage(xray_image)
+        # xray_array = np.flip(np.squeeze(xray_array), axis=0)
+        xray_array = np.rot90(np.squeeze(xray_array)) # make the image upright but NOTE that it is flipped with respect to the y-axis
+        # xray_array = np.squeeze(xray_array) # make the image upright but NOTE that it is flipped with respect to the y-axis
 
-    np_image = (xray_array - xray_array.min()) / (xray_array.max() - xray_array.min()) * 255
-    np_image = np_image.astype(np.uint8)  # Convert to uint8 for PIL compatibility
-    rgb_image = np.stack([np_image] * 3, axis=-1)  # Shape: (H, W, 3)
-    rgb_image = Image.fromarray(rgb_image, mode="RGB")
-    # rgb_image.show()
+        np_image = (xray_array - xray_array.min()) / (xray_array.max() - xray_array.min()) * 255
+        np_image = np_image.astype(np.uint8)  # Convert to uint8 for PIL compatibility
+        rgb_image = np.stack([np_image] * 3, axis=-1)  # Shape: (H, W, 3)
+        rgb_image = Image.fromarray(rgb_image, mode="RGB")
+        # rgb_image.show()
 
-    xray_image = sitk.GetImageFromArray(xray_array)
-    xray_image.SetSpacing((1.0, 1.0))  # Example spacing
-    xray_image.SetOrigin((0.0, 0.0))   # Example origin
+        xray_image = sitk.GetImageFromArray(xray_array)
+        xray_image.SetSpacing((1.0, 1.0))  # Example spacing
+        xray_image.SetOrigin((0.0, 0.0))   # Example origin
 
-    # for ct
-    tensor = torch.tensor(ct_image)
-    # Get the dimensions of the input tensor
-    target_shape = (480,480,240)
+        # for ct
+        tensor = torch.tensor(ct_image)
+        # Get the dimensions of the input tensor
+        target_shape = (480,480,240)
 
-    # Extract dimensions
-    h, w, d = tensor.shape
+        # Extract dimensions
+        h, w, d = tensor.shape
 
-    # Calculate cropping/padding values for height, width, and depth
-    dh, dw, dd = target_shape
-    h_start = max((h - dh) // 2, 0)
-    h_end = min(h_start + dh, h)
-    w_start = max((w - dw) // 2, 0)
-    w_end = min(w_start + dw, w)
-    d_start = max((d - dd) // 2, 0)
-    d_end = min(d_start + dd, d)
+        # Calculate cropping/padding values for height, width, and depth
+        dh, dw, dd = target_shape
+        h_start = max((h - dh) // 2, 0)
+        h_end = min(h_start + dh, h)
+        w_start = max((w - dw) // 2, 0)
+        w_end = min(w_start + dw, w)
+        d_start = max((d - dd) // 2, 0)
+        d_end = min(d_start + dd, d)
 
-    # Crop or pad the tensor
-    tensor = tensor[h_start:h_end, w_start:w_end, d_start:d_end]
+        # Crop or pad the tensor
+        tensor = tensor[h_start:h_end, w_start:w_end, d_start:d_end]
 
-    pad_h_before = (dh - tensor.size(0)) // 2
-    pad_h_after = dh - tensor.size(0) - pad_h_before
+        pad_h_before = (dh - tensor.size(0)) // 2
+        pad_h_after = dh - tensor.size(0) - pad_h_before
 
-    pad_w_before = (dw - tensor.size(1)) // 2
-    pad_w_after = dw - tensor.size(1) - pad_w_before
+        pad_w_before = (dw - tensor.size(1)) // 2
+        pad_w_after = dw - tensor.size(1) - pad_w_before
 
-    pad_d_before = (dd - tensor.size(2)) // 2
-    pad_d_after = dd - tensor.size(2) - pad_d_before
+        pad_d_before = (dd - tensor.size(2)) // 2
+        pad_d_after = dd - tensor.size(2) - pad_d_before
 
-    tensor = torch.nn.functional.pad(tensor, (pad_d_before, pad_d_after, pad_w_before, pad_w_after, pad_h_before, pad_h_after), value=-1)
-    tensor = tensor.permute(2, 0, 1)
-    tensor = tensor.unsqueeze(0)
+        tensor = torch.nn.functional.pad(tensor, (pad_d_before, pad_d_after, pad_w_before, pad_w_after, pad_h_before, pad_h_after), value=-1)
+        tensor = tensor.permute(2, 0, 1)
+        tensor = tensor.unsqueeze(0)
 
-    # save the ct image as a pt tensor
-    torch.save(tensor, ct_save_path) # save as .pt file # np.savez(save_path, resized_array)
-    
-    # save the xray as a .mha image
-    sitk.WriteImage(xray_image, xray_save_path)
+        # save the ct image as a pt tensor
+        torch.save(tensor, ct_save_path) # save as .pt file # np.savez(save_path, resized_array)
+        shutil.rmtree(file_path) #NOTE: remove the raw file after preprocessed the ct img
 
-    # save the xray image as .png image
-    rgb_image.save(xray_rgb_save_path)
+        # save the xray as a .mha image
+        sitk.WriteImage(xray_image, xray_save_path)
 
+        # save the xray image as .png image
+        rgb_image.save(xray_rgb_save_path)
 
-def process(nii_path, shared_dest, split):
+    except:
+        with open('./defect_ct.txt', 'a') as file:
+            file.write(f'{original_file_name}\n')
+
+def process(nii_path, shared_dest, split, num_workers=8):
     assert split in ['train', 'valid']
     nii_files = read_nii_files(nii_path)
 
@@ -222,8 +229,13 @@ def process(nii_path, shared_dest, split):
     df = pd.read_csv('/mnt/c/Users/MaxYo/OneDrive/Desktop/MBP/Chris/CT-CLIP/dataset/metadata/dataset_metadata_validation_metadata.csv') if split == 'valid' \
         else pd.read_csv('/mnt/c/Users/MaxYo/OneDrive/Desktop/MBP/Chris/CT-CLIP/dataset/metadata/train_metadata.csv')
 
-    size = nii_files
-    for k, file in enumerate(nii_files):
-        if k % 10 == 0:
-            print(f'    processing raw CT {k}/{size}')
-        convert_ct_to_xray(file, df, split, shared_dest)
+    # size = len(nii_files)
+    # for k, file in enumerate(nii_files):
+    #     if k % 10 == 0:
+    #         print(f'    processing raw CT {k}/{size}')
+    #     convert_ct_to_xray(file, df, split, shared_dest)
+
+    # Process files using multiprocessing with tqdm progress bar
+    with Pool(num_workers) as pool:
+        func_with_arg = partial(convert_ct_to_xray, df=df, split=split, shared_dst_dir=shared_dest)
+        list(tqdm(pool.imap_unordered(func_with_arg, nii_files), total=len(nii_files)))
