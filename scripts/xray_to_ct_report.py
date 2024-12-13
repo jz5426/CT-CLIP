362bb25b3763ee59df560ed3d3ba4e63c3b6a822
Max You <maxxyouu@mac.home>
2024-12-13 13:54:08 -0500

    file for retrieval

---
+++ scripts/xray_to_ct_report.py
@@ -0,0 +1,7 @@
+"""
+TODO:
+repeat the following and accumulate the stats:
+    1. forward pass a synethic xray for each CT image in the validation set, to the pretrained xray encoder from ULIP style training
+    2. with the embedding from xray encoder, compare it with the existing embeddings of the CT images based the text_embeddings.pth and find the top-k accuracy
+        - note that since we forward pass the syntheic xray of examing ct image, we know the ground truth based on the dictionary id from text_embeddings.
+"""
