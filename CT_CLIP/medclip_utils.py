e88e43b899aefe97e672457a3eac7fa11247f923
Max You <maxxyouu@Maxs-Laptop-2.local>
2025-01-23 15:09:40 -0500

    new baseline files

---
+++ CT_CLIP/medclip_utils.py
@@ -0,0 +1,96 @@
+
+import os
+import torch
+from torch import nn
+import torchvision
+from transformers import AutoModel
+
+class MedCLIPVisionModel(nn.Module):
+    '''
+    take resnet50 as backbone.
+    '''
+    def __init__(self, checkpoint=None, medclip_checkpoint=None):
+        super().__init__()
+        self.model = torchvision.models.resnet50(pretrained=True)
+        num_fts = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_fts, 512, bias=False) # projection head
+        self.WEIGHTS_NAME = 'pytorch_model.bin'
+        if checkpoint is not None:
+            state_dict = torch.load(os.path.join(checkpoint, self.WEIGHTS_NAME))
+            missing_keys, unexpected_keys = self.load_state_dict(state_dict, strict=False)
+            print('missing keys:', missing_keys)
+            print('unexpected keys:', unexpected_keys)
+            print('load model weight from:', checkpoint)
+        if medclip_checkpoint is not None:
+            self.load_from_medclip(medclip_checkpoint)
+        
+        # follow like cxr_clip
+        del self.model.fc
+
+    def load_from_medclip(self, checkpoint):
+        '''handle key mismatch of medclip and the vision encoder.
+        '''
+        state_dict = torch.load(os.path.join(checkpoint, self.WEIGHTS_NAME))
+        new_state_dict = {}
+        for key in state_dict.keys():
+            if 'vision_model' in key:
+                new_state_dict[key.replace('vision_model.','')] = state_dict[key]
+        missing_keys, unexpected_keys = self.load_state_dict(new_state_dict, strict=False)
+        print('missing keys:', missing_keys)
+        print('unexpected keys:', unexpected_keys)
+        print('load model weight from:', checkpoint)
+
+    def forward(self, pixel_values, **kwargs):
+        '''args:
+        pixel_values: tensor with shape [bs, 3, img_size, img_size]
+        '''
+        if pixel_values.shape[1] == 1: pixel_values = pixel_values.repeat((1,3,1,1))
+        img_embeds = self.model(pixel_values)
+        return img_embeds
+
+
+class MedCLIPVisionModelViT(nn.Module):
+    '''take an VIT model as the backbone.
+    '''
+    def __init__(self, checkpoint=None, medclip_checkpoint=None) -> None:
+        '''args:
+        checkpoint: load from the vision encoder checkpoint
+        medclip_checkpoint: load from the vision-text dual encoders checkpoint
+        '''
+        super().__init__()
+        self.vit_type = 'microsoft/swin-tiny-patch4-window7-224' # constants.VIT_TYPE
+        self.model = AutoModel.from_pretrained(self.vit_type)
+        # self.projection_head = nn.Linear(768, 512, bias=False)
+        self.WEIGHTS_NAME = 'pytorch_model.bin'
+        if checkpoint is not None:
+            state_dict = torch.load(os.path.join(checkpoint, self.WEIGHTS_NAME))
+            missing_keys, unexpected_keys = self.load_state_dict(state_dict, strict=False)
+            print('missing keys:', missing_keys)
+            print('unexpected keys:', unexpected_keys)
+            print('load model weight from:', checkpoint)
+        if medclip_checkpoint is not None:
+            self.load_from_medclip(medclip_checkpoint)
+
+    def load_from_medclip(self, checkpoint):
+        '''handle key mismatch of medclip and the vision encoder.
+        '''
+        state_dict = torch.load(os.path.join(checkpoint, self.WEIGHTS_NAME))
+        new_state_dict = {}
+        for key in state_dict.keys():
+            if 'vision_model' in key:
+                new_state_dict[key.replace('vision_model.','')] = state_dict[key]
+        missing_keys, unexpected_keys = self.load_state_dict(new_state_dict, strict=False)
+        print('missing keys:', missing_keys)
+        print('unexpected keys:', unexpected_keys)
+        print('load model weight from:', checkpoint)
+
+    def forward(self, pixel_values, project=True):
+        '''args:
+        pixel_values: tensor with shape [bs, 3, img_size, img_size]
+        '''
+        if pixel_values.shape[1] == 1: pixel_values = pixel_values.repeat((1,3,1,1))
+        output = self.model(pixel_values)
+        img_embeds = output['pooler_output']
+        # if project:
+        #     img_embeds = self.projection_head(img_embeds)
+        return img_embeds
