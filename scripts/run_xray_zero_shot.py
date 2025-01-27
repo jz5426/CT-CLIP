6c27855239b2f64c4d232ebd4c716a52684b652c
jz5426 <max.you@outlook.com>
2025-01-27 10:21:14 -0500

    draft

---
+++ scripts/run_xray_zero_shot.py
@@ -0,0 +1,7 @@
+"""
+TODO:
+      - load the text encoder from the CT-CLIP (no freedom on this)
+      - load the vision encoder either the CT vision encoder or any Xray vision encoder
+      - load the dataset that the text+vision encoder supposed to be on.
+      - zero inference run function with metric evaluation
+"""
