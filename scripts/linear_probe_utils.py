f03fec4e15cf61f98ab71282bce0f2535bb0f8ba
maxxyouu <max.you@outlook.com>
2025-02-16 17:44:47 -0500

    progress

--- scripts/linear_probe_utils.py
+++ scripts/linear_probe_utils.py
@@ -149,7 +149,7 @@ def get_train_internal_split(cfg_dot, cfg):
             'train_dataset': train_dataset,
             'internal_val_dataset': internal_val_dataset
         }
-    elif cfg_dot.linear_probing_params.evaluation_dataset in [const.RADCHEST_CT_PURE_INTERNAL, const.RADCHEST_CT_INTERNAL, const.RADCHEST_CT_PURE_INTERNAL_CLEAN, const.RADCHEST_CT_INTERNAL_CLEAN]:
+    elif cfg_dot.linear_probing_params.evaluation_dataset in [const.RADCHEST_CT_PURE_INTERNAL, const.RADCHEST_CT_INTERNAL, const.RADCHEST_CT_PURE_INTERNAL_CLEAN, const.RADCHEST_CT_INTERNAL_CLEAN, const.RADCHEST_CT_ALL_DISEASE_INTERNAL]:
         print(f'Splitting {cfg_dot.linear_probing_params.evaluation_dataset} dataset')
     
         dataset = cfg_dot.linear_probing_params.evaluation_dataset
@@ -167,6 +167,8 @@ def get_train_internal_split(cfg_dot, cfg):
             labels = '/cluster/projects/mcintoshgroup/publicData/RADChestCT/final_labels_pure_clean.csv' 
         elif dataset == const.RADCHEST_CT_INTERNAL_CLEAN:
             labels = '/cluster/projects/mcintoshgroup/publicData/RADChestCT/final_labels_clean.csv' 
+        elif dataset == const.RADCHEST_CT_ALL_DISEASE_INTERNAL:
+            labels = '/cluster/projects/mcintoshgroup/publicData/RADChestCT/final_labels_all_disease.csv' #TODO: double check this
 
         data_splitter = RadChestXraySplitter(
             labels=labels,
@@ -344,6 +346,93 @@ def get_pathologies(dataset='ct-rate'):
             'bronchial_wall_thickening',
             'bronchiectasis',
             'septal_thickening'
+        ]
+    elif dataset == const.RADCHEST_CT_ALL_DISEASE_INTERNAL:
+
+        pathologies = [
+            'bandlike_or_linear',
+            'groundglass',
+            'honeycombing',
+            'reticulation',
+            'tree_in_bud',
+            'airspace_disease',
+            'air_trapping',
+            'aspiration',
+            'atelectasis',
+            'bronchial_wall_thickening',
+            'bronchiectasis',
+            'bronchiolectasis',
+            'bronchiolitis',
+            'bronchitis',
+            'emphysema',
+            'hemothorax',
+            'interstitial_lung_disease',
+            'lung_resection',
+            'mucous_plugging',
+            'pleural_effusion',
+            'pleural_thickening',
+            'pneumonia',
+            'pneumonitis',
+            'pneumothorax',
+            'pulmonary_edema',
+            'septal_thickening',
+            'tuberculosis',
+            # 'cabg',
+            'cardiomegaly',
+            'coronary_artery_disease',
+            'heart_failure',
+            # 'heart_valve_replacement',
+            # 'pacemaker_or_defib',
+            'pericardial_effusion',
+            'pericardial_thickening',
+            # 'sternotomy',
+            'arthritis',
+            'atherosclerosis',
+            'aneurysm',
+            # 'breast_implant',
+            # 'breast_surgery',
+            'calcification',
+            'cancer',
+            # 'catheter_or_port',
+            'cavitation',
+            # 'clip',
+            'congestion',
+            'consolidation',
+            'cyst',
+            'debris',
+            'deformity',
+            'density',
+            'dilation_or_ectasia',
+            'distention',
+            'fibrosis',
+            'fracture',
+            'granuloma',
+            # 'hardware', #
+            'hernia',
+            'infection',
+            'infiltrate',
+            'inflammation',
+            'lesion',
+            'lucency',
+            'lymphadenopathy',
+            'mass',
+            'nodule',
+            'nodulegr1cm',#
+            'opacity',
+            'plaque',
+            # 'postsurgical',
+            'scarring',
+            'scattered_calc',
+            'scattered_nod',
+            'secretion',
+            'soft_tissue',
+            # 'staple',
+            # 'stent',
+            # 'suture',
+            # 'transplant',
+            # 'chest_tube',
+            # 'tracheal_tube',
+            # 'gi_tube',
         ]
     return pathologies
 
@@ -547,7 +636,7 @@ def evaluate_classifier(params):
             'pretrained_cpt_dest': best_ckpt_destination, # where to retrieve the best checkpoint
         }
         return test_loop(test_params)
-    elif dataset in [const.RADCHEST_CT_INTERNAL, const.RADCHEST_CT_PURE_INTERNAL, const.RADCHEST_CT_INTERNAL_CLEAN, const.RADCHEST_CT_PURE_INTERNAL_CLEAN]:
+    elif dataset in [const.RADCHEST_CT_INTERNAL, const.RADCHEST_CT_PURE_INTERNAL, const.RADCHEST_CT_INTERNAL_CLEAN, const.RADCHEST_CT_PURE_INTERNAL_CLEAN, const.RADCHEST_CT_ALL_DISEASE_INTERNAL]:
         test_dataset = params['test_data']
         print(f'size of the external radchest_ct data: {len(test_dataset)}')
 
