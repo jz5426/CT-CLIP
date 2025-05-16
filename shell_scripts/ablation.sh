dd386b0ec69102c04200f5becbce9358d3ff72bd
jz5426 <max.you@outlook.com>
2025-05-16 13:54:45 -0400

    comments

--- shell_scripts/ablation.sh
+++ shell_scripts/ablation.sh
@@ -16,10 +16,24 @@ source activate ctclip
 ##  here you put the python command line to run the code for training or hyperparameter search
 
 # use_pretrained_xray_encoder = true
-# iterate through different set of weighting parameters
-python /cluster/home/t135419uhn/CT-CLIP/scripts/run_train.py training_params.batch_style=experiment training_params.min_epochs=50 training_params.training_pretrain_baseline=cxr_clip_swin training_params.epochs=51 training_params.use_pretrained_xray_encoder=true training_params.ct_cl_weight=1 training_params.text_cl_weight=0 training_params.loss_function=infoNCE training_params.projector_type=infoNCE
-python /cluster/home/t135419uhn/CT-CLIP/scripts/run_train.py training_params.batch_style=experiment training_params.min_epochs=50 training_params.training_pretrain_baseline=cxr_clip_resnet training_params.epochs=51 training_params.use_pretrained_xray_encoder=true training_params.ct_cl_weight=1 training_params.text_cl_weight=0 training_params.loss_function=infoNCE training_params.projector_type=infoNCE
-python /cluster/home/t135419uhn/CT-CLIP/scripts/run_train.py training_params.batch_style=experiment training_params.min_epochs=50 training_params.training_pretrain_baseline=cxr_clip_swin training_params.epochs=51 training_params.use_pretrained_xray_encoder=true training_params.ct_cl_weight=0 training_params.text_cl_weight=1 training_params.loss_function=infoNCE training_params.projector_type=infoNCE
-python /cluster/home/t135419uhn/CT-CLIP/scripts/run_train.py training_params.batch_style=experiment training_params.min_epochs=50 training_params.training_pretrain_baseline=cxr_clip_resnet training_params.epochs=51 training_params.use_pretrained_xray_encoder=true training_params.ct_cl_weight=0 training_params.text_cl_weight=1 training_params.loss_function=infoNCE training_params.projector_type=infoNCE
-#TODO: before start the training, modify the script so that it save at the 500 epochs instead of override the existing one. train longer the better.
-## train with custom pretrained weights
+gammas=(0.1 0.2 0.4 0.8 1)
+betas=(0.1 0.2 0.4 0.8 1)
+
+# Iterate over all combinations of gamma and beta
+for gamma in "${gammas[@]}"; do
+    for beta in "${betas[@]}"; do
+        echo "Running with gamma=${gamma} and beta=${beta}"
+        
+        python /cluster/home/t135419uhn/CT-CLIP/scripts/run_train.py \
+            training_params.batch_style=experiment \
+            training_params.min_epochs=50 \
+            training_params.training_pretrain_baseline=cxr_clip_swin \
+            training_params.epochs=51 \
+            training_params.use_pretrained_xray_encoder=true \
+            training_params.ct_cl_weight=${gamma} \
+            training_params.text_cl_weight=${beta} \
+            training_params.checkpoint_saving_directory='/cluster/projects/mcintoshgroup/CT-RATE-CHECKPOINTS/Ablation' \
+            training_params.loss_function=infoNCE \
+            training_params.projector_type=infoNCE
+    done
+done
